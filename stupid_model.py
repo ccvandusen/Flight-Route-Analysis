bb7daf200646f216e0018e75dccb36ceb379d71f
ChrisV <francis.vandusen@coloradocollege.edu>
2017-03-23 18:04:27 -0700

    changes to stupid_model variables

--- stupid_model.py
+++ stupid_model.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from datetime import datetime
+import multiprocessing
 
 
 def get_accuracy_score(X_train, X_test, y_train, y_test, num_trees, max_features):
@@ -10,8 +12,7 @@ def get_accuracy_score(X_train, X_test, y_train, y_test, num_trees, max_features
     OUTPUT: score, number of trees
     '''
     rf = RandomForestClassifier(
-        n_estimators=num_trees, max_features=max_features)
-    rf.fit(X_train, y_train)
+        n_estimators=num_trees, max_features=max_features, n_jobs=-1)
     y_pred = rf.predict(X_test)
     return rf.score(X_test, y_test), max_features
 
@@ -19,12 +20,15 @@ def get_accuracy_score(X_train, X_test, y_train, y_test, num_trees, max_features
 def get_and_group_data(filepath):
     data = pd.read_csv(filepath)
     groupby = data.groupby(['route']).mean()
+    groupby = groupby[np.isfinite(groupby['Closure'])]
     y = groupby['Closure']
     del groupby['Closure']
-    X = groupby
+    X = groupby[['ArrDelay', 'DepDelay', 'Distance', 'NASDelay']]
     return X, y
 
 if __name__ == '__main__':
-    X, y = get_and_group_data('test.csv')
+    startTime = datetime.now()
+    X, y = get_and_group_data('data/2004_indicators.csv')
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-    print get_accuracy_score(X_train, X_test, y_train, y_test, 1000, 6)
+    print get_accuracy_score(X_train, X_test, y_train, y_test, 10000, 'log2')
+    print datetime.now() - startTime
